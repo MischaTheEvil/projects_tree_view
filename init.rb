fff9ce812804d77310d9f56f537e23af67f44273
MischaTheEvil <mischa_the_evil@hotmail.com>
2009-08-02 23:51:48 +0200

    Improved documentation and licensing, and prepared 0.0.3 release.

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ end
 Redmine::Plugin.register :projects_tree_view do
   name 'Projects Tree View plugin'
   author 'Chris Peterson'
-  description 'Will turn the projects page into a tree view'
+  description 'This is a Redmine plugin which will turn the projects page into a tree view'
   version '0.0.2'
 end
 
